f20192766ba825be419548ba983509d0c666cf03
edson.fonseca@techne.com.br <edson.fonseca@techne.com.br>
2018-05-25 18:06:29 +0000

    teste

---
+++ src/main/mobileapp/www/js/blockly/Bloco.blockly.js
@@ -0,0 +1,10 @@
+window.blockly = window.blockly || {};
+window.blockly.js = window.blockly.js || {};
+window.blockly.js.blockly = window.blockly.js.blockly || {};
+window.blockly.js.blockly.Bloco = window.blockly.js.blockly.Bloco || {};
+
+/**
+ * Bloco
+ */
+window.blockly.js.blockly.Bloco.Executar = function() {
+}
